1539227f790bd0edb70c6b93fb1741b3dcb0ccdb
sudekidesu <1409870309@qq.com>
2024-05-04 19:03:44 +0800

    init project

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from app import login
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import redirect, url_for, flash, render_template
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app import app
+from app.forms import LoginForm
+from app.models import User
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
